fc5a0ec9690351573d7abf932f644950a681abbb
SriMadhanGokul <srimadhangokul@gmail.com>
2026-03-28 19:13:37 +0530

    final deploy setup

--- backend/server.js
+++ backend/server.js
@@ -82,8 +82,20 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 5000;
+// mongoose
+//   .connect(process.env.MONGO_URI)
+//   .then(() => {
+//     console.log("✅ MongoDB connected");
+//     app.listen(PORT, () => {
+//       console.log(`🚀 Server running on http://localhost:${PORT}`);
+//       console.log(`🌐 Origins: ${allowedOrigins.join(", ")}`);
+//     });
+//   })
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log("Mongo Error:", err));
-console.log("ENV CHECK:", process.env.MONGO_URI);
+  // .catch(err => console.log("Mongo Error:", err));
+  .catch((err) => {
+    console.error("❌ MongoDB error:", err.message);
+    process.exit(1);
+  });
