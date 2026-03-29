570c566e0cd4ab2d8e45b6803efb322a3249a446
SriMadhanGokul <srimadhangokul@gmail.com>
2026-03-29 23:01:43 +0530

    added cors

--- skill-spark/src/utils/api.ts
+++ skill-spark/src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://dordod.onrender.com",
+  baseURL: "https://dordod-1.onrender.com",
   withCredentials: true,
   headers: { "Content-Type": "application/json" },
 });
