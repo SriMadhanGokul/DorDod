597bf83a8f63204a9e58b94e743aff62e4d4fa1f
SriMadhanGokul <srimadhangokul@gmail.com>
2026-06-06 22:33:53 +0530

    Fixed NetworkPage filename case issue

--- skill-spark/src/utils/api.ts
+++ skill-spark/src/utils/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5000/api",
-  //baseURL: "https://dordod-1.onrender.com/api",
+  //baseURL: "http://localhost:5000/api",
+  baseURL: "https://dordod-1.onrender.com/api",
   withCredentials: true,
   headers: { "Content-Type": "application/json" },
 });
