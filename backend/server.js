7fec3183e468b2428402c389c416c6ed79d1aea5
SriMadhanGokul <srimadhangokul@gmail.com>
2026-06-07 19:14:01 +0530

    Fixed NetworkPage filename case issue

--- backend/server.js
+++ backend/server.js
@@ -53,6 +53,7 @@ const allowedOrigins = [
   "http://localhost:8080",
   "http://localhost:3000",
   "https://dordod-2.onrender.com",
+  "https://teal-salmon-657864.hostingersite.com/login",
   process.env.CLIENT_URL,
 ].filter(Boolean);
 
